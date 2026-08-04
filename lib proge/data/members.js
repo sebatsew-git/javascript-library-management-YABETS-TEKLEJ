5a4b233c122ba2cb54f5240e5547e2aae77e28e9
yabets <sebatsew@gmail.com>
2026-08-05 01:02:43 +0300

    add existing changes

---
+++ lib proge/data/members.js
@@ -0,0 +1,26 @@
+export const members = [
+  {
+    id: 101,
+    firstName: "John",
+    lastName: "Doe",
+    email: "john@example.com",
+    phone: "0912345678",
+    borrowedBooks: []
+  },
+  {
+    id: 102,
+    firstName: "Jane",
+    lastName: "Smith",
+    email: "jane@example.com",
+    phone: "0923456789",
+    borrowedBooks: []
+  },
+  {
+    id: 103,
+    firstName: "Alice",
+    lastName: "Johnson",
+    email: "alice@example.com",
+    phone: "0934567890",
+    borrowedBooks: []
+  }
+];
